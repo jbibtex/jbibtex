575a5e6cd80d97aeca27f7eb56264ec2c1d0c5c9
Villu Ruusmann <villu.ruusmann@gmail.com>
2012-02-08 18:02:46 +0000

    String literal styles
    git-svn-id: http://java-bibtex.googlecode.com/svn/trunk@3 50f93694-02f7-6aae-fd95-1ee160778e4b

--- src/main/java/org/jbibtex/StringValue.java
+++ src/main/java/org/jbibtex/StringValue.java
@@ -5,7 +5,53 @@ package org.jbibtex;
 
 public class StringValue extends LiteralValue {
 
-	public StringValue(String string){
+	private Style style = null;
+
+
+	public StringValue(String string, Style style){
 		super(string);
+
+		setStyle(style);
+	}
+
+	public Style getStyle(){
+		return this.style;
+	}
+
+	private void setStyle(Style style){
+		this.style = style;
+	}
+
+	static
+	public enum Style {
+		BRACED("{", "}"),
+		QUOTED("\"", "\""),
+		;
+
+		private String begin = null;
+
+		private String end = null;
+
+
+		Style(String begin, String end){
+			setBegin(begin);
+			setEnd(end);
+		}
+
+		public String getBegin(){
+			return this.begin;
+		}
+
+		private void setBegin(String begin){
+			this.begin = begin;
+		}
+
+		public String getEnd(){
+			return this.end;
+		}
+
+		private void setEnd(String end){
+			this.end = end;
+		}
 	}
 }
